f60242562f2aef74e3b653d026fb0fcab744f6af
meijianming <mjm13@126.com>
2024-12-20 18:03:41 +0800

    修改实现

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,6 +1,7 @@
 
 const Constant={
     QUERY_BOOKMARKS:"queryBookmarks",
+    SAVE_TO_D1:"saveToD1",
     DOWNLOAD_BOOKMARKS:"downloadBookmarks",
     QUERY_FOLDER:"queryFolder",
     STATISTICS_TOTAL:"statisticsTotal",
